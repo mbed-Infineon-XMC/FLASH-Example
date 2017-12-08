d772d99db5dc94e37a9d9cb26e54fc3165deb354
mbed-Infineon-XMC <es16m013@technikum-wien.at>
2017-12-08 21:15:39 +0100

    Add Flash Example

--- main.cpp
+++ main.cpp
@@ -16,30 +16,68 @@
 
 /******************************************************************* Includes */
 #include "mbed.h"
+#include "rtos.h"
 
 /******************************************************************** Globals */
+
+InterruptIn button(SW2);
+DigitalIn button2(SW1);
+DigitalOut led1(LED1);
+DigitalOut led2(LED2);
+FlashIAP nvm;
+volatile uint8_t cnt[256] = { 0 };
+volatile uint8_t local_cnt;
+
 Serial pc(P0_5, P0_4); // tx, rx (9600 8N1)
 
+/****************************************************************** Callbacks */
+
+/**
+ * Callback if rising edge detected
+ */
+void rise() {
+
+    local_cnt++;
+    led1 = !led1;
+}
+
 /****************************************************************** Functions */
+
+void nvm_thread()
+{
+    /* Load data from Flash after power up */
+    uint32_t addr = nvm.get_flash_start();
+    nvm.read((void*)&cnt[0], addr, sizeof(cnt));
+    local_cnt = cnt[0];
+
+    while (true) {
+        /* Wait for press event */
+        if (!button2) {
+            led2 = 1;
+            cnt[0] = local_cnt;
+            /* Save new data into flash */
+            nvm.erase(addr, nvm.get_sector_size(addr));
+            nvm.program((void*)cnt, addr, sizeof(cnt));
+            led2 = 0;
+        }
+        wait(0.2);
+    }
+}
 
 /**
  * Main Function
  */
 int main() {
-    set_time(1512652152);  // Set RTC time to Thursday, December 7, 2017 1:09:12 PM
+
+    Thread worker;
+    worker.start(callback(nvm_thread));
+
+    /* Attach rising callback function */
+    button.rise(&rise);
 
     while (true) {
-        time_t seconds = time(NULL);
-
-        pc.printf("Time as seconds since January 1, 1970 = %d\n", seconds);
-
-        pc.printf("Time as a basic string = %s", ctime(&seconds));
-
-        char buffer[32];
-        strftime(buffer, 32, "%I:%M %p\n", localtime(&seconds));
-        pc.printf("Time as a custom formatted string = %s", buffer);
-
         wait(1);
+        pc.printf("Button was pressed %d times!\n", local_cnt);
     }
 }
 
